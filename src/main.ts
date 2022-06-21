768bbf38f4546fe06a4625da6e43e78e0dc8afd5
Monica Dias <msumiedias@gmail.com>
2022-06-21 09:55:30 -0300

    chore: implement user

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,10 @@ async function bootstrap() {
     .setTitle('PizzaFresh')
     .setDescription('Aplicação para gestão das mesas de uma pizzaria')
     .setVersion('1.0.0')
-    .addTag('status') // < A ORDEM PODE SER ALTERADA
-    .addTag('table') // < A ORDEM PODE SER ALTERADA
-    .addTag('product') // < A ORDEM PODE SER ALTERADA
+    .addTag('status')
+    .addTag('table')
+    .addTag('product')
+    .addTag('user') // < NOVA TAG
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
