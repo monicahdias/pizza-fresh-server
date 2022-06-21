f28b098249e1f43a1e847e657e75a0e202dd429a
Monica Dias <msumiedias@gmail.com>
2022-06-20 23:45:12 -0300

    chore: implement prisma

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/create-table.dto';
 import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
-  tables: Table[] = [];
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return this.tables;
+  findAll(): Promise<Table[]> {
+    return this.prisma.table.findMany();
   }
 
-  create(createTableDto: CreateTableDto) {
-    const table: Table = { id: 'random_id', ...createTableDto };
+  findOne(id: string): Promise<Table> {
+    return this.prisma.table.findUnique({ where: { id } });
+  }
 
-    this.tables.push(table);
+  create(dto: CreateTableDto): Promise<Table> {
+    const data: Table = { ...dto };
 
-    return table;
+    return this.prisma.table.create({ data });
   }
 }
